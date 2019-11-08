d8a0d75543738813a1fe8bce2722391c68d6b5ca
Teodor <41966270+TeodorIonut001@users.noreply.github.com>
2019-11-08 16:03:33 +0200

    Add files via upload

---
+++ Main/Drive.java
@@ -0,0 +1,21 @@
+package launcher;
+import java.io.IOException;
+//import map.RoadMap;
+import gui.Input;
+public class Drive {
+	public static void main(String[] args) 
+	//Exceptions
+	//OutputFile exception:
+	throws IOException 
+	{
+//		//Main program
+		Input inputInterface = new Input();
+		inputInterface.inputPage();
+		
+		//Test RoadMap
+//		RoadMap road = new RoadMap();
+		
+		
+		
+	}
+}
